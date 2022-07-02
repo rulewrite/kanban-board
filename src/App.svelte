aa7fc85993afe0445ddc37e4a92f68626f35113b
rulewrite <rulewrite@gmail.com>
2022-07-03 04:21:11 +0900

    feat: material ui 적용

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,9 @@
 <script lang="ts">
+  import Button from '@smui/button';
+  import IconButton from '@smui/icon-button';
 </script>
 
-<main>hello!</main>
+<main>
+  <Button>TEST BUTTON</Button>
+  <IconButton class="material-icons">build</IconButton>
+</main>
