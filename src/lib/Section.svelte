76f6c08c3f89a3e86449a1b1737bcd277837115a
rulewrite <rulewrite@gmail.com>
2022-07-10 04:16:46 +0900

    refactor: 섹션 컴포넌트 분리

---
+++ src/lib/Section.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import Paper, { Subtitle, Title } from '@smui/paper';
+  import type { Section } from './api/api';
+  import { mapKeyToEntities } from './store/entities';
+
+  const sections = mapKeyToEntities.sections;
+
+  export let id: Section['id'];
+</script>
+
+<div class="placeholder">
+  <Paper>
+    {#if id}
+      {@const section = $sections[id]}
+      <Title>{section.title}</Title>
+      <Subtitle>{section.body}</Subtitle>
+    {/if}
+  </Paper>
+</div>
+
+<style>
+  .placeholder {
+    margin-right: 16px;
+  }
+</style>
--- src/lib/Sections.svelte
+++ src/lib/Sections.svelte
@@ -1,10 +1,8 @@
 <script lang="ts">
-  import Paper, { Subtitle, Title } from '@smui/paper';
   import { onMount } from 'svelte';
   import { sectionApi } from './api/api';
-  import { mapKeyToEntities } from './store/entities';
+  import Section from './Section.svelte';
 
-  const sections = mapKeyToEntities.sections;
   let status: ReturnType<typeof sectionApi.readList>;
 
   onMount(() => {
@@ -14,13 +12,7 @@
 
 <div class="wrapper">
   {#each $status?.cargo ?? [] as id (id)}
-    {@const section = $sections[id]}
-    <div class="placeholder">
-      <Paper>
-        <Title>{section.title}</Title>
-        <Subtitle>{section.body}</Subtitle>
-      </Paper>
-    </div>
+    <Section {id} />
   {/each}
 </div>
 
@@ -30,9 +22,5 @@
     flex-direction: row;
     align-items: stretch;
     min-height: 800px;
-  }
-
-  .placeholder {
-    margin-right: 16px;
   }
 </style>
