8cbba3a72ca2b1a3fc204f45ea98f8f99d72f67d
rulewrite <rulewrite@gmail.com>
2022-07-09 19:34:11 +0900

    feat: 섹션 목록 컴포넌트 UI 추가

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
   import Main from './lib/Main.svelte';
+  import Sections from './lib/Sections.svelte';
   import Topbar from './lib/Topbar.svelte';
 </script>
 
 <Topbar />
-<Main>hello world</Main>
+<Main>
+  <Sections />
+</Main>
---
+++ src/lib/Sections.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+  import Paper, { Subtitle, Title } from '@smui/paper';
+
+  let sections = [
+    {
+      id: 1,
+      title:
+        'sunt aut facere repellat provident occaecati excepturi optio reprehenderit',
+      body: 'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto',
+      userId: 1,
+    },
+    {
+      id: 2,
+      title: 'qui est esse',
+      body: 'est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla',
+      userId: 1,
+    },
+  ];
+</script>
+
+<div class="wrapper">
+  {#each sections as section}
+    <div class="placeholder">
+      <Paper>
+        <Title>{section.title}</Title>
+        <Subtitle>{section.body}</Subtitle>
+      </Paper>
+    </div>
+  {/each}
+</div>
+
+<style>
+  .wrapper {
+    display: flex;
+    flex-direction: row;
+    align-items: stretch;
+    min-height: 800px;
+  }
+
+  .placeholder {
+    margin-right: 16px;
+  }
+</style>
