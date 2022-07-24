a7707ff4388f34d6bb86834fabac8bfe46fa1779
rulewrite <rulewrite@gmail.com>
2022-07-24 18:29:48 +0900

    refactor: 정렬 액션 드롭 이벤트 droppable 사용하도록 변경

--- src/lib/actions/arrange/arrange.ts
+++ src/lib/actions/arrange/arrange.ts
@@ -1,3 +1,8 @@
+import {
+  dropEntityEventType,
+  droppable,
+  Parameter as DroppableParameter,
+} from '../droppable';
 import OrderedPosition, { Id, Position } from './OrderedPosition';
 import { draggable, dragging } from './style';
 
@@ -11,6 +16,38 @@ export interface Arrangeable {
   position: Position;
   id: number;
 }
+
+const dragenter: DroppableParameter['dragenter'] = (event, $dragging) => {
+  if ($dragging === event.currentTarget) {
+    return;
+  }
+
+  $sibling = event.currentTarget;
+  const isNext = $sibling.nextElementSibling === $dragging;
+  $sibling.parentNode.insertBefore(
+    $dragging,
+    isNext ? $sibling : $sibling.nextElementSibling
+  );
+};
+
+const handleDropEntity = () => {
+  if (!$sibling) {
+    return;
+  }
+
+  $dragging.dispatchEvent(
+    new CustomEvent<DropPositionEvent['detail']>('dropPosition', {
+      detail: {
+        siblingId: Number($sibling.dataset.id),
+        position: orderedPosition.getBetween(
+          $dragging[groupIdKey],
+          $dragging.previousElementSibling === $sibling,
+          Number($sibling.dataset.position)
+        ),
+      },
+    })
+  );
+};
 
 const mapEventTypeToListener = new Map<string, EventListener>([
   [
@@ -32,74 +69,6 @@ const mapEventTypeToListener = new Map<string, EventListener>([
       event.currentTarget.classList.remove(dragging);
       $dragging = null;
       $sibling = null;
-    },
-  ],
-  [
-    // 드래그 중인 대상이 적합한 드롭 대상위에 올라갔을 때
-    'dragenter',
-    (event: HTMLElementIncludeDragEvent) => {
-      event.stopPropagation();
-
-      if ($dragging === event.currentTarget) {
-        return;
-      }
-
-      if ($dragging[groupIdKey] !== event.currentTarget[groupIdKey]) {
-        return;
-      }
-
-      $sibling = event.currentTarget;
-      const isNext = $sibling.nextElementSibling === $dragging;
-      $sibling.parentNode.insertBefore(
-        $dragging,
-        isNext ? $sibling : $sibling.nextElementSibling
-      );
-    },
-  ],
-  [
-    // 드래그 중인 대상이 적합한 드롭 대상 위에 있을 때 (수백 ms 마다 발생)
-    'dragover',
-    (event: HTMLElementIncludeDragEvent) => {
-      /**
-       * https://developer.mozilla.org/ko/docs/Web/API/HTML_Drag_and_Drop_API/Drag_operations#droptargets
-       * 앱의 대부분의 영역은 적합한 드롭 대상이 아니므로 기본적으로 드롭을 허용하지 않도록 되어있음
-       * 따라서 드롭을 허용하려면 기본 처리를 막아야 함.
-       */
-      event.preventDefault();
-      event.stopPropagation();
-      return false;
-    },
-  ],
-  [
-    // 드래그 중인 대상을 적합한 드롭 대상에 드롭했을 때
-    'drop',
-    (event: HTMLElementIncludeDragEvent) => {
-      /**
-       * https://developer.mozilla.org/ko/docs/Web/API/HTML_Drag_and_Drop_API/Drag_operations#drop
-       * 웹페지에서 드롭을 수락한 경우 기본 브라우저의 처리도 막아야 함.
-       * 예로 링크를 웹페이지로 드래그하면 브라우저가 링크로 리다이렉션 됨.
-       */
-      event.preventDefault();
-      event.stopPropagation();
-
-      if (!$sibling) {
-        return;
-      }
-
-      $dragging.dispatchEvent(
-        new CustomEvent<DropPositionEvent['detail']>('dropPosition', {
-          detail: {
-            siblingId: Number($sibling.dataset.id),
-            position: orderedPosition.getBetween(
-              $dragging[groupIdKey],
-              $dragging.previousElementSibling === $sibling,
-              Number($sibling.dataset.position)
-            ),
-          },
-        })
-      );
-
-      return false;
     },
   ],
 ] as const);
@@ -126,6 +95,12 @@ export function arrange(node: HTMLElement, parameter: Parameter | null) {
   set(node, parameter);
   orderedPosition.add(groupId, position);
 
+  node.addEventListener(dropEntityEventType, handleDropEntity);
+  const { update: updateDroppable, destroy: destroyDroppable } = droppable(
+    node,
+    { groupId, dragenter }
+  );
+
   mapEventTypeToListener.forEach((listener, eventType) => {
     node.addEventListener(eventType, listener);
   });
@@ -134,9 +109,14 @@ export function arrange(node: HTMLElement, parameter: Parameter | null) {
     update(updatedParameter: Parameter) {
       set(node, updatedParameter);
       orderedPosition.replace(groupId, position, updatedParameter.position);
+
+      updateDroppable({ groupId: updatedParameter.groupId, dragenter });
     },
     destroy() {
       orderedPosition.remove(groupId, Number(node.dataset.position));
+
+      node.removeEventListener(dropEntityEventType, handleDropEntity);
+      destroyDroppable();
 
       mapEventTypeToListener.forEach((listener, eventType) => {
         node.removeEventListener(eventType, listener);
--- src/lib/actions/droppable.ts
+++ src/lib/actions/droppable.ts
@@ -2,9 +2,11 @@ import { $dragging, groupIdKey } from './arrange/arrange';
 
 const droppableGroupId = Symbol('droppableGroupId');
 const dragenterKey = Symbol('dragenter');
+export const dropEntityEventType = 'dropEntity';
 
 const mapEventTypeToListener = new Map<string, EventListener>([
   [
+    // 드래그 중인 대상이 적합한 드롭 대상위에 올라갔을 때
     'dragenter',
     (event: HTMLElementIncludeDragEvent) => {
       event.stopPropagation();
@@ -23,21 +25,33 @@ const mapEventTypeToListener = new Map<string, EventListener>([
     },
   ],
   [
+    // 드래그 중인 대상이 적합한 드롭 대상 위에 있을 때 (수백 ms 마다 발생)
     'dragover',
     (event: HTMLElementIncludeDragEvent) => {
+      /**
+       * https://developer.mozilla.org/ko/docs/Web/API/HTML_Drag_and_Drop_API/Drag_operations#droptargets
+       * 앱의 대부분의 영역은 적합한 드롭 대상이 아니므로 기본적으로 드롭을 허용하지 않도록 되어있음
+       * 따라서 드롭을 허용하려면 기본 처리를 막아야 함.
+       */
       event.preventDefault();
       event.stopPropagation();
       return false;
     },
   ],
   [
+    // 드래그 중인 대상을 적합한 드롭 대상에 드롭했을 때
     'drop',
     (event: HTMLElementIncludeDragEvent) => {
+      /**
+       * https://developer.mozilla.org/ko/docs/Web/API/HTML_Drag_and_Drop_API/Drag_operations#drop
+       * 웹페지에서 드롭을 수락한 경우 기본 브라우저의 처리도 막아야 함.
+       * 예로 링크를 웹페이지로 드래그하면 브라우저가 링크로 리다이렉션 됨.
+       */
       event.preventDefault();
       event.stopPropagation();
 
       event.currentTarget.dispatchEvent(
-        new CustomEvent<DropEntityEvent['detail']>('dropEntity', {
+        new CustomEvent<DropEntityEvent['detail']>(dropEntityEventType, {
           detail: { id: Number($dragging.dataset.id) },
         })
       );
