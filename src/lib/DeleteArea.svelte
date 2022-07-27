a682859c54d4d2e3f170983edd9d704fa5263c26
rulewrite <rulewrite@gmail.com>
2022-07-27 23:20:22 +0900

    feat: 삭제 영역 플로팅으로 변경

--- src/lib/DeleteArea.svelte
+++ src/lib/DeleteArea.svelte
@@ -1,33 +1,34 @@
 <script lang="ts">
-  import Ripple from '@smui/ripple';
+  import Fab, { Icon, Label } from '@smui/fab';
   import { onDestroy } from 'svelte';
   import type { Unsubscriber } from 'svelte/store';
-  import { slide } from 'svelte/transition';
   import { dragging } from './actions/draggable';
   import { dragentered, droppable, Parameter } from './actions/droppable';
   import { cardApi, sectionApi } from './api/jsonPlaceholder';
   import { groupId as cardGroupId } from './Card.svelte';
   import { groupId as sectionGroupId } from './Section.svelte';
 
-  let deleteArea: HTMLDivElement;
-  let active = false;
+  let isHover = false;
   let deleteUnsubscribe: Unsubscriber;
 
   const dragenter: Parameter['dragenter'] = (e, $dragging) => {
     $dragging.hidden = true;
-
-    active = true;
-    deleteArea.dispatchEvent(new MouseEvent('mousedown'));
+    isHover = true;
   };
 
   const dragleave: Parameter['dragleave'] = (e, $dragging) => {
     $dragging.hidden = false;
+    isHover = false;
 
     dragentered.clear();
-    active = false;
   };
 
   const deleteEntity: Parameter['drop'] = (e, dragging) => {
+    // exited 애니메이션으로 사라지는 시간 만큼 지연
+    setTimeout(() => {
+      isHover = false;
+    }, 500);
+
     const api =
       dragging.getProps().groupId === cardGroupId ? cardApi : sectionApi;
 
@@ -49,34 +50,32 @@
   });
 </script>
 
-{#if $dragging}
-  <div transition:slide>
-    <div
-      id="trashCan"
-      bind:this={deleteArea}
-      use:Ripple={{ surface: true, active }}
-      use:droppable={{
+<Fab
+  class="delete"
+  color={isHover ? 'primary' : 'secondary'}
+  exited={!$dragging}
+  extended
+  use={[
+    [
+      droppable,
+      {
         groupIds: [sectionGroupId, cardGroupId],
         dragenter,
         dragleave,
         drop: deleteEntity,
-      }}
-    >
-      <b>Delete Area</b>
-    </div>
-  </div>
-{/if}
+      },
+    ],
+  ]}
+>
+  <Icon class="material-icons">delete</Icon>
+  <Label>Delete</Label>
+</Fab>
 
 <style>
-  #trashCan {
-    height: 300px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    font-size: 40px;
-    color: #ddd;
-    border: 8px dashed #ddd;
-    box-sizing: border-box;
-    margin-bottom: 20px;
+  :global(.delete) {
+    position: fixed;
+    bottom: 50px;
+    right: 50px;
+    z-index: 1;
   }
 </style>
