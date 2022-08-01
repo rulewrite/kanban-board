98fbe851f52dd66567503b1c608858181f2c7a27
rulewrite <rulewrite@gmail.com>
2022-08-02 03:09:14 +0900

    feat: ids 마크업 변경

--- src/lib/Ids.svelte
+++ src/lib/Ids.svelte
@@ -1,20 +1,13 @@
 <script lang="ts">
+  import Chip, { Set, Text } from '@smui/chips';
+
   export let ids: Array<number> = [];
 </script>
 
 {#if ids.length}
-  <div class="ids">{ids.join(', ')}</div>
+  <Set chips={ids} let:chip nonInteractive>
+    <Chip {chip}>
+      <Text>{chip}</Text>
+    </Chip>
+  </Set>
 {/if}
-
-<style>
-  .ids {
-    font-size: 14px;
-    line-height: 1.3;
-
-    padding: 3px;
-    margin: 10px 0;
-    background: #eee;
-    color: #111;
-    border-radius: 3px;
-  }
-</style>
