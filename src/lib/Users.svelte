932e5dd0bd1165c597d585fbbeab2e30822a34b6
rulewrite <rulewrite@gmail.com>
2022-08-02 02:04:17 +0900

    feat: 유저 삭제 추가

--- src/lib/Users.svelte
+++ src/lib/Users.svelte
@@ -1,8 +1,10 @@
 <script lang="ts" context="module">
-  import Chip, { Set, Text } from '@smui/chips';
-  import { onMount } from 'svelte';
-  import { userApi } from './api/jsonPlaceholder';
+  import Chip, { Set, Text, TrailingIcon } from '@smui/chips';
+  import { onDestroy, onMount } from 'svelte';
+  import type { Unsubscriber } from 'svelte/store';
+  import { User, userApi } from './api/jsonPlaceholder';
   import { isDeleted, mapKeyToEntities } from './store/entities';
+  import { unsubscribeErrorHandler } from './utils';
 
   const users = mapKeyToEntities.users;
 </script>
@@ -15,14 +17,44 @@
     .map((id) => userEntities[id])
     .filter((user) => !user?.[isDeleted]);
 
+  const unsubscribers: Array<Unsubscriber> = [];
+
   onMount(() => {
     status = userApi.readList({ params: {} });
   });
+
+  onDestroy(() => {
+    try {
+      unsubscribers.forEach((unsubscriber) => unsubscriber());
+    } catch (error) {
+      unsubscribeErrorHandler(error);
+    }
+  });
+
+  const deleteUser = (id: User['id']) => {
+    const unsubscriber = userApi
+      .delete({ id })
+      .subscribe(({ isFetching, failMessage }) => {
+        if (isFetching) {
+          return;
+        }
+
+        if (failMessage) {
+          return;
+        }
+
+        unsubscriber();
+      });
+    unsubscribers.push(unsubscriber);
+  };
 </script>
 
 <Set chips={validUsers} let:chip={user} key={(user) => user.id}>
   <Chip chip={user.id}>
     <Text>{user.email}</Text>
+    <TrailingIcon class="material-icons" on:click={() => deleteUser(user.id)}>
+      cancel
+    </TrailingIcon>
   </Chip>
 </Set>
 
